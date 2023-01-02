125eb9dfd7c13badbe7454b710f652c2b842a278
ferhat <84874779+f6c5@users.noreply.github.com>
2023-01-02 17:25:32 +0300

    Delete script.js

--- script.js
+++
@@ -1,7 +0,0 @@
-//alert("daha yapım aşamasında");
-let d = document;
-let wrap = d.querySelector('.wrap');
-let items = d.querySelector('.items');
-let itemCount = d.querySelectorAll('.item').length;
-let pos = 0;
-let transform = Modernizr.prefixed('transform');
